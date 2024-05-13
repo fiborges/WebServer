18bc2e0da98f15f1469dd318647a594dfcb7a988
BrunoAlone <brolivei@student.42porto.com>
2024-05-13 15:39:18 +0100

    Big files problem resolved!

--- CGI_Test_01/main.cpp
+++ CGI_Test_01/main.cpp
@@ -41,20 +41,6 @@ void send_html_page(int client_socket)
 
 void	print_the_request(std::string& total_request)
 {
-	// std::cout << "\n\n===============WRITING ONE TEST===============\n\n";
-
-	// std::string	testString;
-
-	// for (int i = 0; i < 10; i++)
-	// {
-	// 	if (i == 2)
-	// 		testString.append(1, '\0');
-	// 	else
-	// 		testString.append(1, 'a' + i);
-	// }
-
-	// std::cout << testString << std::endl;
-
 	std::cout << "\n\n===============END POINT OF THE TEST===============\n\n";
 
 	std::cout << "\n\n===============WRITING ALL THE REQUEST IN THE SCREEN===============\n\n";
@@ -76,6 +62,40 @@ void	print_the_request(std::string& total_request)
 
 }
 
+int	verify_body_content(std::string request)
+{
+	long unsigned int	content_length;
+	size_t	pos = request.find("Content-Length: ");
+
+	std::string	tmp;
+	int	i = 0;
+
+	while (isdigit(request[pos + 16]))
+	{
+		tmp[i] = request[pos + 16];
+		i++;
+		pos++;
+	}
+
+	content_length = atoi(tmp.c_str());
+
+	std::cout << "The content length found was " << content_length << std::endl;
+
+	std::string	boundary;
+
+	if ((pos = request.find("boundary=")) != std::string::npos)
+	{
+		tmp.append(request, (request.find("\r\n\r\n") + 4));
+
+		std::cout << "After boundary:]" << tmp << "[Finish";
+
+		if (tmp.length() < content_length)
+		 return (-1);
+	}
+
+	return (0);
+}
+
 std::string	read_all_body(int client_socket)
 {
 	char		line[1024];
@@ -83,18 +103,25 @@ std::string	read_all_body(int client_socket)
 
 	while (1)
 	{
-		memset(line, 0, 1024);
-		ssize_t bytesRead = read(client_socket, line, 1023);
-
-		if (bytesRead < 0)
+		while (1)
 		{
-			std::cerr << "Error in read\n";
-			exit(-1);
+			memset(line, 0, 1024);
+			ssize_t bytesRead = read(client_socket, line, 1023);
+
+			if (bytesRead < 0)
+			{
+				std::cerr << "Error in read\n";
+				exit(-1);
+			}
+
+			total_request.append(line, bytesRead);
+
+			if (bytesRead < 1023)
+				break;
 		}
-
-		total_request.append(line, bytesRead);
-
-		if (bytesRead < 1023)
+		if (verify_body_content(total_request) < 0)
+			continue;
+		else
 			break;
 	}
 
@@ -154,10 +181,14 @@ int	main(int argc, char **argv, char **envp)
 
 		buffer_in_string = read_all_body(new_socket);
 
+		//if (verify_body_content(buffer_in_string) < 0)
+		//	continue;
+
 		if (buffer_in_string.find("GET /index.html") != std::string::npos)
 			send_html_page(new_socket);
 		else if (buffer_in_string.find("POST /cgi-bin") != std::string::npos)
-			cgi.PerformCGI(new_socket, buffer_in_string);
+			std::cout << "CUCU\n";
+			//cgi.PerformCGI(new_socket, buffer_in_string);
 		else if (buffer_in_string.find("GET /close_server") != std::string::npos)
 		{
 			close(new_socket);
