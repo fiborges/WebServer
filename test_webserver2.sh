01c1e1ff21f20671f4d60703c222536ad9830bd3
Bruno Joel Oliveira Soares <brolivei@c2r5s9.42porto.com>
2024-07-16 12:00:58 +0100

    Modification in Tester

--- test_webserver2.sh
+++ test_webserver2.sh
@@ -32,27 +32,27 @@ print_test_case "GET request to uploads directory" "Should return the contents o
 curl -X GET http://localhost:8080/uploads/
 
 ((test_num++))
-# [3] OK 
+# [3] OK
 # POST Requests
 print_test_case "POST request to upload a file" "Should successfully upload a file."
 curl -X POST -F "file=@./resources/website/aaa.txt" http://localhost:8080/uploads/
 
 ((test_num++))
-# [4] OK 
+# [4] OK
 # DELETE Requests
 print_test_case "DELETE request to remove a file" "Should delete the specified file."
 curl -X DELETE http://localhost:8080/uploads/aaa.txt
 
 ((test_num++))
-# [5] OK 
+# [5] OK
 # CGI Script Execution
 print_test_case "GET request to execute ExampleGET.py CGI script" "Should execute the Python CGI script and return its output."
-curl -X GET http://localhost:8080/cgi-bin/ExampleGET.py
+curl -X GET "http://localhost:8080/cgi-bin/ExampleGET.py?num1=5&num2=10&num3=15"
 
 ((test_num++))
 # [6] OK
 print_test_case "POST request to execute ExampleGET.py CGI script" "Should execute the Python CGI script with POST data and return its output."
-curl -X POST -d "num1=0&num2=30&num3=4" http://localhost:8080/cgi-bin/ExampleGET.py
+curl -X POST -d "num1=6&num2=30&num3=4" http://localhost:8080/cgi-bin/ExampleGET.py
 
 ((test_num++))
 # [7] OK
